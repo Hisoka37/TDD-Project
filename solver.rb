64aeccb6f02e66302147100bcddbd964154907b6
Walid Kabboussa <walidkabou@gmail.com>
2023-06-01 15:51:20 +0100

    Add fizzbuzz method and fix linters

--- solver.rb
+++ solver.rb
@@ -1,11 +1,24 @@
 class Solver
-  def factorial(n)
-    raise 'Input must be zero or a positive number' if n < 0
-    return 1 if n <= 1
-    return n * factorial(n-1)
+  def factorial(num)
+    raise 'Input must be zero or a positive number' if num.negative?
+    return 1 if num <= 1
+
+    num * factorial(num - 1)
   end
 
   def reverse(word)
     word.reverse
+  end
+
+  def fizzbuzz(num)
+    if ((num % 5) + (num % 3)).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
   end
 end
--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -15,7 +15,7 @@ describe Solver do
     end
 
     it 'should raise an exception when a negative number is passed' do
-      expect{subject.factorial(-4)}.to raise_error 'Input must be zero or a positive number'
+      expect { subject.factorial(-4) }.to raise_error 'Input must be zero or a positive number'
     end
   end
   context '#reverse' do
@@ -25,19 +25,16 @@ describe Solver do
   end
   context '#fizzbuzz' do
     it 'should return fizz when Number is divisible by 3' do
-      expect(subject.fizzbuzz(3)).eq 'fizz'
+      expect(subject.fizzbuzz(3)).to eq 'fizz'
     end
     it 'should return buzz when Number is divisible by 5' do
-      expect(subject.fizzbuzz(25)).eq 'buzz'
+      expect(subject.fizzbuzz(25)).to eq 'buzz'
     end
     it 'should return fizzbuzz when Number is divisible by 3 and 5' do
-      expect(subject.fizzbuzz(15)).eq 'fizz'
+      expect(subject.fizzbuzz(15)).to eq 'fizzbuzz'
     end
     it 'should return Number as string when Number is not divisible by 3 or 5' do
-      expect(subject.fizzbuzz(7)).eq '7'
+      expect(subject.fizzbuzz(7)).to eq '7'
     end
   end
-
 end
-
-
