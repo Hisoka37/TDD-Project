1645bbdaee10e7501df27e2903658fd0baed2a0b
Precious Betine <preciousbetine@gmail.com>
2023-06-01 15:04:55 +0100

    Write tests for factorial method

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,5 +1,21 @@
 require_relative '../solver'
 
 describe Solver do
+  context '#factorial' do
+    it 'should return 1 when 0 is passed' do
+      expect(subject.factorial(0)).to eq 1
+    end
 
+    it 'should return 2 when 2 is passed' do
+      expect(subject.factorial(2)).to eq 2
+    end
+
+    it 'should return 120 when 5 is passed' do
+      expect(subject.factorial(5)).to eq 120
+    end
+
+    it 'should raise an exception when a negative number is passed' do
+      expect(subject.factorial(-4)).to raise_error(StandardError, 'Input must be zero or a positive number')
+    end
+  end
 end
